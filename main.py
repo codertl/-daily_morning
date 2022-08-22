0b740cb5c7a4b3c0da2683227f2ecac7159ea276
codertl <78476735+codertl@users.noreply.github.com>
2022-08-22 23:05:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,39 +17,69 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-print('参数:',app_id,app_secret,birthday,city,start_date,template_id,user_id)
+print('参数:', app_id, app_secret, birthday, city, start_date, template_id, user_id)
+
+
 def get_weather():
-  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-  res = requests.get(url).json()
-  weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+    url = "https://v0.yiketianqi.com/api?unescape=1&version=v91&appid=43656176&appsecret=I42og6Lm&ext=&cityid=&city=" + city
+    res = requests.get(url).json()
+    weather = res['data'][0]
+    return weather['wea'], \
+           math.floor(weather['tem1']) , \
+           math.floor(weather['tem2']), \
+           weather['date'] + ' '+ weather['week'], \
+           weather['win'][0],weather['win_speed'],\
+           weather['win_meter'],\
+           weather['humidity'],\
+           weather['visibility'],\
+           weather['air'],\
+           weather['air_level']
+
 
 def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
+    delta = today - datetime.strptime(start_date, "%Y-%m-%d")
+    return delta.days
+
 
 def get_birthday():
-  print('get_birthday',birthday)
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  print('get_birthday',next)
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+    print('get_birthday', birthday)
+    next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+    print('get_birthday', next)
+    if next < datetime.now():
+        next = next.replace(year=next.year + 1)
+    return (next - today).days
+
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
-  if words.status_code != 200:
-    return get_words()
-  return words.json()['data']['text']
+    words = requests.get("https://api.shadiao.pro/chp")
+    if words.status_code != 200:
+        return get_words()
+    return words.json()['data']['text']
+
 
 def get_random_color():
-  return "#%06x" % random.randint(0, 0xFFFFFF)
+    return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, tem1, tem2, current_date, win,win_speed,win_meter,humidity,visibility,air,air_level = get_weather()
+data = {
+    "city":{"value": city,"color": get_random_color()}, # 城市
+    "date": {"value": current_date,"color": get_random_color()}, #日期
+    "weather": {"value": wea,"color": get_random_color()}, # 天气
+    "high_temperature": {"value": tem1,"color": '#E3170D'}, #高温
+    "low_temperature": {"value": tem2,"color": '#FF6347'}, #低温
+    "win": {"value": win,"color": '#1E90FF'}, #风向
+    "win_speed": {"value": win_speed,"color": '#1E90FF'}, # 风力等级
+    "win_meter": {"value": win_meter,"color": '#1E90FF'}, #风速
+    "humidity": {"value": humidity,"color": '#1E90FF'}, #湿度
+    "visibility": {"value": visibility,"color": '#1E90FF'}, #能见度
+    "air": {"value": air,"color": '#1E90FF'}, #空气质量
+    "air_level": {"value": air_level,"color": '#1E90FF'}, #空气质量等级
+    "love_days": {"value": get_count()},
+    "birthday_left": {"value": get_birthday(), "color": get_random_color()}, # 生日
+    "words": {"value": get_words(), "color": get_random_color()}
+}
 res = wm.send_template(user_id, template_id, data)
-print(res)
